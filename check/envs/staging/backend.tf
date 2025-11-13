f817f67a8d1b9d247e1c2aa29d9eea66e8839003
Karuch <talk159820@gmail.com>
2025-11-13 14:45:42 +0200

    removed terraform apply from cli (use backend.tf instead), fix the wroking-directory not initiallize envs thing, now need to test both environements

--- check/envs/staging/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "project-generator-test-backend"
-    key            = "terraform/check/staging/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "project-generator-test-lock"
-    encrypt        = true
-  }
-}
